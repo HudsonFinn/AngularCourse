191f1ad423b85869560024a3bc09004c9b5ed7ba
Finn Hudson <11finnh@gmail.com>
2020-08-21 20:01:17 +0100

    feat: Added first curstom component and exported it to a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
