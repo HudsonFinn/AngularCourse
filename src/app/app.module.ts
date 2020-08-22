dc2bedbb76161b0d02f9e8cfbe0e7af315d9b350
Finn Hudson <11finnh@gmail.com>
2020-08-22 15:44:23 +0100

    feat: Added custom convertToSpaces pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   declarations: [
     AppComponent,
-		ProductListComponent
+		ProductListComponent,
+		ConvertToSpacesPipe
   ],
   bootstrap: [
 		AppComponent
